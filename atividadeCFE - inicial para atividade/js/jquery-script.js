3f56ce7daafea3e97ba76d0f699b9f9584f7e210
Carlos A Gama <95866417+CarlosGama115@users.noreply.github.com>
2023-06-14 21:53:13 -0300

    atividadeCFE - inicial para atividade
    #2Exercício de Fronte End. Realizado. 
    #OBS Página da aula.

---
+++ atividadeCFE - inicial para atividade/js/jquery-script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $("#botao-cadastrar").click(function(){
+        $("#forms-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+       
+    })
+})
+
